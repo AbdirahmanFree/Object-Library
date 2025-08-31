ba505024c7e7024d798b6d93cfd975e5d1397e7e
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-08-31 19:25:03 -0400

    Added modal form to add book

--- main.js
+++ main.js
@@ -15,6 +15,29 @@ function addBookToLibrary(title, author, genre, pages, read,) {
     library.push(book)
 }
 
-addBookToLibrary("The Lord of The Rings", "J.R.R Tolkien", "Fantasy", "300", false)
 
-console.log(library)
+/// Pull up add button modal
+const openBtn = document.getElementById("open-modal");
+const closeBtn = document.getElementById("close-modal")
+const modal = document.getElementById("modal")
+
+openBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+  });
+closeBtn.addEventListener("click", () => {
+
+})
+
+modal.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = e.currentTarget
+    const data = Object.fromEntries(new FormData(form).entries())
+    const title = data.title.trim();
+    const author = data.author.trim();
+    const pages = Number(data.pages);
+    const genre = data.genre;
+    addBookToLibrary(title, author, genre, pages, false)
+    console.log(library)
+    modal.classList.remove("open")
+})
+
